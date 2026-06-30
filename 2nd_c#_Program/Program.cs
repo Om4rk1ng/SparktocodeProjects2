d338b897d3266351964252b90b3c0479ada2704d
Omar Al-Balushi <om9763@gmail.com>
2026-06-30 16:54:17 +0400

    inputting and displaying

---
+++ 2nd_c#_Program/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _2nd_c__Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //inputting name and displaying it
+
+            Console.Write("enter your name please:");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello " + name);
+
+           
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

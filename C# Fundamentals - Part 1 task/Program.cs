3ec6fd9fd5d6742dc565b1158a5a9a798740bd9a
Omar Al-Balushi <om9763@gmail.com>
2026-07-01 12:53:50 +0400

    task 9 done

--- C# Fundamentals - Part 1 task/Program.cs
+++ C# Fundamentals - Part 1 task/Program.cs
@@ -169,6 +169,22 @@
 
 
             }
+            //task 9: Day Name Finder///////////////////////////////////////////////////////////
+
+            Console.WriteLine("Enter a number between 1 and 7 representing the day: ");
+            int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+            switch (dayNumber)
+            {
+                case 1: Console.WriteLine("sunday"); break;
+                case 2: Console.WriteLine("monday"); break;
+                case 3: Console.WriteLine("tuesday"); break;
+                case 4 : Console.WriteLine("wednesday"); break;
+                case 5: Console.WriteLine("thursday"); break;
+                case 6: Console.WriteLine("friday"); break;
+                case 7: Console.WriteLine("saturday"); break;
+                default: Console.WriteLine("Invalid day number"); break;
+            }
 
         }
     }

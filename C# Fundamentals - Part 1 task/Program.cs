de78ff60e7826e5d1eb4ca0dbcb6caca0dc66a95
Omar Al-Balushi <om9763@gmail.com>
2026-07-01 10:20:16 +0400

    task 1 is complete

---
+++ C# Fundamentals - Part 1 task/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace C__Fundamentals___Part_1_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1: Personal Info Card
+
+            
+            string Name=Console.ReadLine();
+            int Age = Convert.ToInt32(Console.ReadLine());
+            double Height = Convert.ToDouble(Console.ReadLine());
+            bool IsStudent = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Height: " + Height);
+            Console.WriteLine("Student: " + IsStudent);
+        }
+    }
+}

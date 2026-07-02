f5a323b1f4b7ac97f5e2065dbc6724f0cba05f22
Omar Al-Balushi <om9763@gmail.com>
2026-07-02 12:15:38 +0400

    task 1 done

---
+++ c# fundamentals- part 2 task 2 loops_error_handling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace c__fundamentals__part_2_task_2_loops_error_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////Task 1 Countdown Timer/////////////////////////////////////////////
+            ///
+
+            Console.WriteLine("enter a countdown starting number: ");
+
+            for (int countdown = Convert.ToInt32(Console.ReadLine()); countdown > 0; countdown--)
+            {
+                Console.WriteLine(countdown);
+            }
+
+            
+        }
+    }
+}

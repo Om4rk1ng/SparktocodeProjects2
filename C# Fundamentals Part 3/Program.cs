d7d9855453918bcfe3a84087e44593e67351c593
Omar Al-Balushi <om9763@gmail.com>
2026-07-05 15:40:04 +0400

    task 1 done

--- C# Fundamentals Part 3/Program.cs
+++ C# Fundamentals Part 3/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ///////Task 1 Absolute Difference/////////////////////////////////////////////
+            ///
+
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int SubResult = Math.Abs(num2 - num1);
+
+            Console.WriteLine("Absolute value of the subtraction result: " + SubResult);
+
+
         }
     }
 }

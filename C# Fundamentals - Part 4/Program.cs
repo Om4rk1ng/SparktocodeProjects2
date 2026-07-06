74e51f8d475d6bd317ce93d08b01aadce6c628fe
Omar Al-Balushi <om9763@gmail.com>
2026-07-06 16:15:12 +0400

    task 1 done

--- C# Fundamentals - Part 4/Program.cs
+++ C# Fundamentals - Part 4/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace C__Fundamentals___Part_4
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace C__Fundamentals___Part_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ///////Task 1 Absolute Difference/////////////////////////////////////////////
+            ///
+
+            Console.WriteLine("enter your name: ");
+            String name = Console.ReadLine();
+
+            static void PrintWelcome(string name)
+            {
+                Console.WriteLine($"Hello, {name}!");
+            }
+
+            PrintWelcome(name);
+
+
+
         }
     }
 }

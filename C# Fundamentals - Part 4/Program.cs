a2edd0eb3d3a2b566b46b1d17edcaca1134019c2
Omar Al-Balushi <om9763@gmail.com>
2026-07-06 17:09:03 +0400

    task 7 done and organized functions separately

--- C# Fundamentals - Part 4/Program.cs
+++ C# Fundamentals - Part 4/Program.cs
@@ -7,6 +7,79 @@ namespace C__Fundamentals___Part_4
 {
     internal class Program
     {
+        //task 1 - Welcome Message Function
+        public static void PrintWelcome(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+
+        //task 2 - Square Number Function
+
+        public static int SquareNumber(int num)
+        {
+            return num * num;
+        }
+
+        //task 3 - Celsius to Fahrenheit Function
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        //task 4 - Fixed Menu Display Function
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. start");
+            Console.WriteLine("2. help");
+            Console.WriteLine("3. Exit");
+
+        }
+
+        //task 5 - Even or Odd Function
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+        //task 6 - Rectangle Area & Perimeter Functions
+        public static double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        public static double CalculatePerimeter(double length, double width)
+        {
+            return 2 * (length + width);
+        }
+
+        //task 7 - Grade Letter Function
+        public static string GetGradeLetter(int grade)
+        {
+            if (grade >= 90)
+            {
+                return "A";
+            }
+            else if (grade >= 80)
+            {
+                return "B";
+            }
+            else if (grade >= 70)
+            {
+                return "C";
+            }
+            else if (grade >= 60)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+
         static void Main(string[] args)
         {
             ///////Task 1 Absolute Difference/////////////////////////////////////////////
@@ -15,10 +88,7 @@ namespace C__Fundamentals___Part_4
             Console.WriteLine("enter your name: ");
             String name = Console.ReadLine();
 
-            static void PrintWelcome(string name)
-            {
-                Console.WriteLine($"Hello, {name}!");
-            }
+            
 
             PrintWelcome(name);
 
@@ -31,10 +101,7 @@ namespace C__Fundamentals___Part_4
             int squaredNumber = SquareNumber(number);
 
 
-            static int SquareNumber(int num)
-            {
-                return num * num;
-            }
+           
 
             Console.WriteLine(SquareNumber(number));
 
@@ -46,24 +113,14 @@ namespace C__Fundamentals___Part_4
             
             double fahrenheit = CelsiusToFahrenheit(celsius);
 
-            static double CelsiusToFahrenheit(double celsius)
-            {
-                return (celsius * 9 / 5) + 32;
-            }
+            
 
             Console.WriteLine("fehrenheit: "+ fahrenheit);
 
             ///////Task 4 -  Fixed Menu Display Function/////////////////////////////////////////////
             ///
 
-            static void DisplayMenu()
-            {
-                Console.WriteLine("Menu:");
-                Console.WriteLine("1. start");
-                Console.WriteLine("2. help");
-                Console.WriteLine("3. Exit");
-
-            }
+            
 
             DisplayMenu();
 
@@ -75,10 +132,7 @@ namespace C__Fundamentals___Part_4
             Console.WriteLine("Enter a number to check if it is even or odd: ");
             int checkNumber = int.Parse(Console.ReadLine());
 
-            static bool IsEven(int num)
-            {
-                return num % 2 == 0;
-            }
+           
 
             if (IsEven(checkNumber))
             {
@@ -97,19 +151,21 @@ namespace C__Fundamentals___Part_4
             Console.WriteLine("Enter the width of the rectangle: ");
             double width = double.Parse(Console.ReadLine());
 
-            static double CalculateArea(double length, double width)
-            {
-                return length * width;
-            }
-
-            static double CalculatePerimeter(double length, double width)
-            {
-                return 2 * (length + width);
-            }
+           
 
             Console.WriteLine("Area: " + CalculateArea(length, width));
             Console.WriteLine("Perimeter: " + CalculatePerimeter(length, width));
 
+            ///////Task 7 -   Grade Letter Function/////////////////////////////////////////////
+            ///
+
+            Console.WriteLine("Enter your grade (0-100): ");
+            int grade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your grade letter is: " + GetGradeLetter(grade));
+
+            //////Task 8 - Countdown Function/////////////////////////////////////////////
+            /// 
 
         }
     }

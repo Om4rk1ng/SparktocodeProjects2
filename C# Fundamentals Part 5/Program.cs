0366572506c07f3ccd2ca9c8ea5ab137113c09c3
Omar Al-Balushi <om9763@gmail.com>
2026-07-07 13:46:11 +0400

    task 1 done

--- C# Fundamentals Part 5/Program.cs
+++ C# Fundamentals Part 5/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+
+            ///////task 1 Fixed Grades Array/////////////////////////////////////////////
+            ///
+
+            int[] numbers = new int[5];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                Console.WriteLine("enter a number to be added to the array of integers: ");
+                int num = int.Parse(Console.ReadLine());
+
+                numbers[i] = num;
+            }
+
+            Console.WriteLine("\n");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
 
 
         }
